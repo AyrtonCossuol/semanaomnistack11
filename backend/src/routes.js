7d17264c8d703cf30fd54cb65c94911e4d824967
AyrtonCossuol <ayrton.cossuol@live.com>
2020-05-26 11:14:57 -0300

    Criando as funcionalidades/rotas do back-end

---
+++ backend/src/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidenteController');
+
+const connection = require('./database/connection');
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
